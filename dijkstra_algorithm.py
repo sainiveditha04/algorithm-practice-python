45d6d7142edd41dd4683236e9af52f03fc685dcd
sainiveditha04 <sainiveditha4@gmail.com>
2026-03-14 16:03:21 +0530

    Implement Dijkstra's algorithm for shortest paths

---
+++ dijkstra_algorithm.py
@@ -0,0 +1,29 @@
+import heapq
+
+graph = {
+    'A': [('B', 1), ('C', 4)],
+    'B': [('C', 2), ('D', 5)],
+    'C': [('D', 1)],
+    'D': []
+}
+
+def dijkstra(start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+
+    pq = [(0, start)]
+
+    while pq:
+        current_distance, current_node = heapq.heappop(pq)
+
+        for neighbor, weight in graph[current_node]:
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+
+    return distances
+
+
+print(dijkstra('A'))
