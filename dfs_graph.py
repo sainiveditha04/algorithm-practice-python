cdb56070f4b776c4363954997230429232b5f712
sainiveditha04 <sainiveditha4@gmail.com>
2026-03-14 16:01:13 +0530

    Add depth-first search implementation for graph traversal
    This implementation of depth-first search (DFS) traverses a graph starting from node 'A', printing each visited node.

---
+++ dfs_graph.py
@@ -0,0 +1,20 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+visited = set()
+
+def dfs(node):
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+
+        for neighbor in graph[node]:
+            dfs(neighbor)
+
+dfs('A')
